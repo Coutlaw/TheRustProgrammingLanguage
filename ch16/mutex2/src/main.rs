aff2c077009db0deecca2ca42a8e6d1335775594
Cass Outlaw <cass.d.outlaw@gmail.com>
2020-05-30 06:21:39 -0400

    mutex lock with Atomic reference counter (Arc)

---
+++ ch16/mutex2/src/main.rs
@@ -0,0 +1,25 @@
+use std::sync::{Mutex, Arc};
+use std::thread;
+
+fn main() {
+    // Arc is like RC but safe for concurrency
+    let counter = Arc::new(Mutex::new(0)); // we have to use an atomic reference counter here because a mutex can only have a single owner
+    let mut handles = vec![];
+
+    // spawn 10 threads that all take the mutex lock to mutate the data inside the lock
+    for _ in 0..10 {
+        let counter = Arc::clone(&counter);
+        let handle = thread::spawn( move || {
+            // take the mutex lock by the thread
+            let mut num = counter.lock().unwrap();
+            *num += 1;
+        });
+        handles.push(handle);
+    }
+
+    // complete all the threads
+    for handle in handles {
+        handle.join().unwrap();
+    }
+    println!("Result: {}", *counter.lock().unwrap());
+}
