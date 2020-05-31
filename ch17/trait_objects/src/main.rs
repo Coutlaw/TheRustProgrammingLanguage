8d99e6c05fb8a8f56ffb92d05781cd999b59a0ae
Cass Outlaw <cass.d.outlaw@gmail.com>
2020-05-31 07:52:14 -0400

    external user extended our gui library thanks to trait objects

--- ch17/trait_objects/src/main.rs
+++ ch17/trait_objects/src/main.rs
@@ -1,3 +1,40 @@
+// just an example if someone were to consume our lib and make a new type of thing to draw on screen
+use trait_objects::{Draw, Screen, Button};
+
 fn main() {
-    println!("Hello, world!");
+    // now a user can consume our screen and add their new type to be drawn on the screen without issue
+    // thanks to trait objects, also known as "duck typing"
+    let screen = Screen {
+        components: vec![
+            Box::new(SelectBox {
+                width: 75,
+                height: 10,
+                options: vec![
+                    String::from("Yes"),
+                    String::from("Maybe"),
+                    String::from("No"),
+                ],
+            }),
+            Box::new(Button {
+                width: 50,
+                height: 10,
+                label: String::from("OK"),
+            }),
+        ],
+    };
+
+    screen.run();
+}
+
+// A user wants to add a new field to draw on the screen from our library
+struct SelectBox {
+    width: u32,
+    height: u32,
+    options: Vec<String>,
+}
+
+impl Draw for SelectBox {
+    fn draw(&self) {
+        // code that would draw it on the screen
+    }
 }
