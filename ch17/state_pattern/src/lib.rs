ec4e859e0dd5efd8edb327ee18caec74650ff4a7
Cass Outlaw <cass.d.outlaw@gmail.com>
2020-06-03 08:11:58 -0400

    fully implemented the blog post state flow

--- ch17/state_pattern/src/lib.rs
+++ ch17/state_pattern/src/lib.rs
@@ -11,10 +11,72 @@ impl Post {
 			content: String::new(),
 		}
 	}
+
+	pub fn add_text(&mut self, text: &str) {
+		self.content.push_str(text);
+	}
+
+	pub fn content(&self) -> &str {
+		self.state.as_ref().unwrap().content(&self)
+	}
+
+	pub fn request_review(&mut self) {
+		if let Some(s) = self.state.take() {
+			self.state = Some(s.request_review())
+		}
+	}
+
+	pub fn approve(&mut self) {
+		if let Some(s) = self.state.take() {
+			self.state = Some(s.approve())
+		}
+	}
 }
 
-trait State {}
+trait State {
+	fn request_review(self: Box<Self>) -> Box<dyn State>;
+	fn approve(self: Box<Self>) -> Box<dyn State>;
+	// default implementation
+	fn content<'a>(&self, post: &'a Post) -> &'a str {
+		""
+	}
+}
 
 struct Draft {}
 
-impl State for Draft {}
+impl State for Draft {
+	fn request_review(self: Box<Self>) -> Box<dyn State> {
+		Box::new(PendingReview {})
+	}
+	fn approve(self: Box<Self>) -> Box<dyn State>{
+		self
+	}
+}
+
+struct PendingReview {}
+
+impl State for PendingReview {
+	fn request_review(self: Box<Self>) -> Box<dyn State>{
+		self
+	}
+	fn approve(self: Box<Self>) -> Box<dyn State> {
+		Box::new(Published {})
+	}
+}
+
+struct Published {}
+
+impl State for Published {
+	fn request_review(self: Box<Self>) -> Box<dyn State> {
+		self
+	}
+
+	fn approve(self: Box<Self>) -> Box<dyn State> {
+		self
+	}
+
+	// we have to have a lifetime since we are taking a reference to a post and we need to return a part of that reference
+	fn content<'a>(&self, post: &'a Post) -> &'a str {
+		&post.content
+	}
+}
