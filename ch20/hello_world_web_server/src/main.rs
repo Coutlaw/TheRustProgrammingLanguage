09cd66cd7db26c82ed290aeee38330bf889e7594
Cass Outlaw <cass.d.outlaw@gmail.com>
2020-06-05 06:46:19 -0400

    http server is listening to incoming requests

--- ch20/hello_world_web_server/src/main.rs
+++ ch20/hello_world_web_server/src/main.rs
@@ -1,3 +1,14 @@
+use std::net::TcpListener;
+
 fn main() {
-    println!("Hello, world!");
+    // 7878 is the default http port, also it is how you spell rust on a 10 digit keypad
+    // our TcpListner will establish the connection string and wait for incoming requests
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+
+    // process the incoming requests here
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+
+        println!("Connection established!");
+    }
 }
