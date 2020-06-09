40738a15c31803bab29c5742d3b4418fa39ff0cc
Coutlaw <cass.d.outlaw@gmail.com>
2020-06-09 06:27:35 -0400

    bad multi threaded solution, limitless threads

--- ch20/hello_world_web_server/src/main.rs
+++ ch20/hello_world_web_server/src/main.rs
@@ -1,6 +1,7 @@
 use std::io::prelude::*;
 use std::net::TcpListener;
 use std::net::TcpStream;
+use std::thread;
 
 fn main() {
     // 7878 is the default http port, also it is how you spell rust on a 10 digit keypad
@@ -10,8 +11,8 @@ fn main() {
     // process the incoming requests here
     for stream in listener.incoming() {
         let stream = stream.unwrap();
-
-        handle_connection(stream);
+        // bad implementation here, we are creating limitless threads for every request incoming
+        thread::spawn(|| handle_connection(stream));
     }
 }
 
@@ -33,7 +34,6 @@ fn handle_connection(mut stream: TcpStream) {
     } else {
         ("HTTP/1.1 404 NOT FOUND\r\n\r\n", "404.html")
     };
-    
     // read file as string
     let contents = fs::read_to_string(filename).unwrap();
 
@@ -42,6 +42,6 @@ fn handle_connection(mut stream: TcpStream) {
 
     // we can send bytes downstream on the connection with .write(&[u8])
     stream.write(response.as_bytes()).unwrap(); // unwrap because .write could return an error
-    // flush() will wait for all bytes to be written before continuing in the program
+                                                // flush() will wait for all bytes to be written before continuing in the program
     stream.flush().unwrap();
 }
