52efa18c70d283c89a4ae1a51d225709971ee6b0
Coutlaw <cass.d.outlaw@gmail.com>
2020-06-12 08:42:27 -0400

    added docs, check with cargo doc --open, and added a panic for creating a thread pool with a less than 0 initial size

--- ch20/hello_world_web_server/lib/thread_pool/src/lib.rs
+++ ch20/hello_world_web_server/lib/thread_pool/src/lib.rs
@@ -1,16 +1,25 @@
 pub struct ThreadPool;
 
 impl ThreadPool {
-    pub fn new(_size: usize) -> ThreadPool {
+    /// Create a new ThreadPool.
+    ///
+    /// The size is the number of threads in the pool.
+    ///
+    /// # Panics
+    ///
+    /// The `new` function will panic if the size is zero
+    pub fn new(size: usize) -> ThreadPool {
+        // TODO: rewrite with Result<ThreadPool, PoolCreationError> at some point
+        assert!(size > 0);
+        
         ThreadPool
     }
 
     pub fn execute<F>(&self, f: F)
-        where 
-            F: FnOnce() + Send + 'static
-        {
-            
-        }
+    where
+        F: FnOnce() + Send + 'static,
+    {
+    }
 }
 
 #[cfg(test)]
