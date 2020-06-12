74352c973ce15e771766da400bcb86ebe8bea9d9
Coutlaw <cass.d.outlaw@gmail.com>
2020-06-12 09:01:31 -0400

    created worker struct that will live in the thread pool to execute tasks as they come in

--- ch20/hello_world_web_server/lib/thread_pool/src/lib.rs
+++ ch20/hello_world_web_server/lib/thread_pool/src/lib.rs
@@ -1,4 +1,9 @@
-pub struct ThreadPool;
+use std::thread;
+
+// Our pool of thread workers
+pub struct ThreadPool {
+    workers: Vec<Worker>,
+}
 
 impl ThreadPool {
     /// Create a new ThreadPool.
@@ -11,8 +16,14 @@ impl ThreadPool {
     pub fn new(size: usize) -> ThreadPool {
         // TODO: rewrite with Result<ThreadPool, PoolCreationError> at some point
         assert!(size > 0);
-        
-        ThreadPool
+
+        let mut workers = Vec::with_capacity(size);
+
+        // create size number of workers for the pool
+        for id in 0..size {
+            workers.push(Worker::new(id))
+        }
+        ThreadPool { workers }
     }
 
     pub fn execute<F>(&self, f: F)
@@ -21,6 +32,24 @@ impl ThreadPool {
     {
     }
 }
+
+struct Worker {
+    id: usize,
+    thread: thread::JoinHandle<()>,
+}
+
+impl Worker {
+    fn new(id: usize) -> Worker {
+        let thread = thread::spawn(|| {});
+
+        Worker{
+            id,
+            thread,
+        }
+    }
+}
+
+
 
 #[cfg(test)]
 mod tests {
